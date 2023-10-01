a2e7c727ad8082d88ea7d2a0744f7abe204ab1f3
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-01 11:00:07 +0200

    finalize deploy script

--- web3/contracts/scripts/deploy.ts
+++ web3/contracts/scripts/deploy.ts
@@ -2,6 +2,7 @@ import { ethers } from "hardhat";
 
 const sepoliaFunctionsRouter = "0xb83E47C2bC239B3bf370bc41e1459A34b41238D0";
 const sepoliaCCIPRouter = "0xD0daae2231E9CB96b94C8512223533293C3693Bf";
+const sepoliaLinkAddress = "0x779877A7B0D9E8603169DdbD7836e478b4624789";
 
 async function main() {
   const carbonLink = await ethers.deployContract("CarbonLink", [sepoliaFunctionsRouter], {});
@@ -9,7 +10,7 @@ async function main() {
   const savedCarbonLink = await carbonLink.waitForDeployment();
 
   const receiver = await ethers.deployContract("Receiver", [sepoliaCCIPRouter, savedCarbonLink.getAddress()], {});
-  const sender = await ethers.deployContract("Sender", [sepoliaCCIPRouter], {});
+  const sender = await ethers.deployContract("Sender", [sepoliaCCIPRouter, sepoliaLinkAddress], {});
 
   await receiver.waitForDeployment();
   await sender.waitForDeployment();
