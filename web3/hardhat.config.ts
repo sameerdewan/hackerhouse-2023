5dcb14ff2a28386e2030380c26ea41f5fed39737
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-01 11:12:20 +0200

    keys

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -3,18 +3,22 @@ import "@nomicfoundation/hardhat-toolbox";
 import '@chainlink/contracts'
 import '@openzeppelin/hardhat-upgrades'
 
+const infuraURL = "https://sepolia.infura.io/v3/df4de1f306044440bf0e4843d8ff667b";
+const privateKey = "cbee8ce12491a420e5f6985be94c95a4305d375d63f7aab74c2148ffb7bf3492"; // DEMO
+const etherscanKey = "RTKTPPWTQKQ6UNTVFRG6VWFN3SCI281IJP";
+
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
 
   networks: {
     sepolia: {
-      url: '', // TODO: add sepolia node url
-      accounts: [process.env.PRIVATE_KEY || ''],
+      url: infuraURL, // TODO: add sepolia node url
+      accounts: [privateKey],
     },
   },
   defaultNetwork: 'sepolia',
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: etherscanKey,
   },
 }
 
