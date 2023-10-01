2579dcb95fecc75ff4bbd990523f618b4437c941
Sameer Dewan <sameerdewan1994@gmail.com>
2023-10-01 10:53:46 +0200

    deployment script work

--- web3/contracts/scripts/deploy.ts
+++ web3/contracts/scripts/deploy.ts
@@ -6,7 +6,13 @@ const sepoliaCCIPRouter = "0xD0daae2231E9CB96b94C8512223533293C3693Bf";
 async function main() {
   const carbonLink = await ethers.deployContract("CarbonLink", [sepoliaFunctionsRouter], {});
 
-  await carbonLink.waitForDeployment();
+  const savedCarbonLink = await carbonLink.waitForDeployment();
+
+  const receiver = await ethers.deployContract("Receiver", [sepoliaCCIPRouter, savedCarbonLink.getAddress()], {});
+  const sender = await ethers.deployContract("Sender", [sepoliaCCIPRouter], {});
+
+  await receiver.waitForDeployment();
+  await sender.waitForDeployment();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
