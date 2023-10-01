3ee356dc1741978d2416938078bb1e7d0bf12a01
ephraim888sun <ephraim888sun@gmail.com>
2023-10-01 11:06:38 +0200

    add hardhat template

--- web3/hardhat.config.ts
+++ web3/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@chainlink/contracts'
+import '@openzeppelin/hardhat-upgrades'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.19',
 
-export default config;
+  networks: {
+    mumbai: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/tCbwTAqlofFnmbVORepuHNcsrjNXWdRJ',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+    sepolia: {
+      url: 'https://ethereum-sepolia.g.alchemy.com/v2/your-sepolia-url',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+  defaultNetwork: 'mumbai',
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+}
+
+export default config
